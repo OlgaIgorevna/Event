a0c136c192bc2bd0b886ca9a571d8510136cb863
Olga Igorevna Pron <olga.pron.kld@gmail.com>
2017-10-31 14:48:40 +0200

    сделана кнопка перемещения наверх страницы

---
+++ script.js
@@ -0,0 +1,6 @@
+/*var layouts= document.getElementsByClassName("layout-positioner");
+//alert(layouts.length);
+alert("сейчас должен поменяться фон");
+layouts[1].style.background = 'red';
+alert('Элемент layout стал красным, а сейчас обратно вернётся');
+layouts[1].style.background = '';*/
